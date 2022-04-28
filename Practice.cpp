5a60c80adae793bb96a72ab97c31fbc9d55da1f0
Max Kuse <mwkuse@mail.csuchico.edu>
2022-04-27 20:40:44 -0700

    Added Unit Testing for all functions in Practice class and fixed bugs in sortDescending

--- Practice.cpp
+++ Practice.cpp
@@ -8,39 +8,42 @@ using std::string;
 void Practice::sortDescending(int & first, int & second, int & third)
 {
   int biggest, middle, smallest;
-  if( second > third && second > first )
-  {
-    biggest = second;
-  }
-  else if( third > second && third > first )
-  {
-    biggest = third;
-  }
-  else
-  {
-    biggest = first;
-  }
-  if( second < third && second < first )
-  {
-    smallest = second;
-  }
-  else if( third < second && third < first )
-  {
-    smallest = third;
-  }
-  else
-  {
-    smallest = first;
-  }
-  if( first != biggest && first != smallest ){
-    middle = first;
-  }
-  else if( second != biggest && second != smallest ){
-    middle = second;
-  }
-  else{
-    middle = third;
-  }
+   if( second > third && second > first )
+   {
+     biggest = second;
+   }
+   else if( third > second && third > first )
+   {
+     biggest = third;
+   }
+   else
+   {
+     biggest = first;
+   }
+   if( second < third && second < first )
+   {
+     smallest = second;
+   }
+   else if( third < second && third < first )
+   {
+     smallest = third;
+   }
+   else
+   {
+     smallest = first;
+   }
+   if( first != biggest && first != smallest ){
+     middle = first;
+   }
+   else if( second != biggest && second != smallest ){
+     middle = second;
+   }
+   else{
+     middle = third;
+   }
+   first = biggest;
+   second = middle;
+   third = smallest;
 }
 
 // Receive a string and return whether or not it is strictly a palindrome,
@@ -49,24 +52,22 @@ void Practice::sortDescending(int & first, int & second, int & third)
 bool Practice::isPalindrome(string input)
 {
   for(unsigned int i=0; i < input.size(); i++)
-  {
-    if( input[i] < 'A' || input[i] > 'Z' )
     {
-      //change lower case to upper case
-      input[i] = input[i] - ('a' - 'A');
+      if( input[i] < 'A' || input[i] > 'Z' )
+      {
+        //change lower case to upper case
+        input[i] = input[i] - ('a' - 'A');
+      }
     }
-  }
-
-  bool match = false;
-
-  for(unsigned int i=0; i < input.size()/2; i++)
-  {
-    if( input[i] == input[input.size()-1-i] )
-      match = true;
-    else
-      match = false;
-  }
-  return match;
+    bool match = false;
+    for(unsigned int i=0; i < input.size()/2; i++)
+    {
+      if( input[i] == input[input.size()-1-i] )
+        match = true;
+      else
+        match = false;
+    }
+    return match;
 }
 
 // This function receives a string and counts how many times the same character
@@ -74,28 +75,32 @@ bool Practice::isPalindrome(string input)
 // function is case sensative so 'Z' is different than 'z'.
 int Practice::count_starting_repeats(string word)
 {
-    int repetition = 0;
-    int index = 0;
-    char letter;
+  int repetition = 0;
+  int index = 0;
+  char letter;
+  if( word.length() > 0 ){
+    letter = word[0];
+    repetition = 1;
+  }
+  for(unsigned int i=1; i < word.length(); i++){
+    if( word[i] == letter ){
+      repetition++;
+    }
+  }
 
-    if( word.length() > 0 )
-      letter = word[0];
-
-    for(unsigned int i=1; i < word.length(); i++){
-      if( word[i] == letter ){
-        repetition++;
-      }
-    }    
-
-    return repetition;
+  return repetition;
 }
 
 // Receives an array that represents the hours someone sleeps each night of the week
 // (as an array of seven integers) and returns a pointer to locate the first instance
-// of an "all nighter" in the array (a day with 0 hours sleep) and returns the pointer. 
+// of an "all nighter" in the array (a day with 0 hours sleep) and returns the pointer.
 // However, if there are no such days found, the function should return nullptr.
 int* Practice::allnighter(int sleep[7])
 {
-
-
+  for(int i = 0; i < 7; i++){
+    if(sleep[i] == 0){
+      return &sleep[i];
+    }
+  }
+  return nullptr;
 }
--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,7 +14,94 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+// Throwing Smoke
 TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
+}
+
+// allnighter Happy Tests
+TEST(PracticeTest, sleep_test0)
+{
+    Practice testObject;
+		int sleep[] = {0,0,0,0,0,0,0};
+		int *ptr = testObject.allnighter(sleep);
+		ASSERT_EQ(ptr, &sleep[0]);
+}
+
+TEST(PracticeTest,sleep_test_na)
+{
+	Practice testObject;
+	int sleep[] = {1,2,3,4,5,6,7};
+	int *ptr = testObject.allnighter(sleep);
+	ASSERT_EQ(ptr, nullptr);
+}
+
+TEST(PracticeTest, sleep_test_mid)
+{
+	Practice testObject;
+	int sleep[] = {1,2,3,0,5,6,7};
+	int *ptr = testObject.allnighter(sleep);
+	ASSERT_EQ(ptr, &sleep[3]);
+}
+
+// allnighter Sad Tests
+TEST(PracticeTest, sleep_ne)
+{
+	Practice testObject;
+	int sleep[] = {};
+	int *ptr = testObject.allnighter(sleep);
+	ASSERT_EQ(ptr, nullptr);
+}
+
+// count_starting_repeats Happy Tests
+TEST(PracticeTest, zzz_repeats)
+{
+	Practice testObject;
+	ASSERT_EQ(testObject.count_starting_repeats("zzz"), 3);
+}
+
+// isPalindrome Happy Tests
+TEST(PracticeTest, racecar_test)
+{
+	Practice testObject;
+	ASSERT_TRUE(testObject.isPalindrome("racecar"));
+}
+
+// isPalindrome Sad Test
+TEST(PracticeTest, exclamition_test)
+{
+	Practice testObject;
+	ASSERT_TRUE(testObject.isPalindrome("!tot!"));
+}
+
+TEST(PracticeTest, double_test)
+{
+	Practice testObject;
+	ASSERT_TRUE(testObject.isPalindrome("aa"));
+}
+// sortDescending Happy Tests
+TEST(PracticeTest, ascending)
+{
+	Practice testObject;
+	int a = 1;
+	int b = 2;
+  int c = 3;
+	testObject.sortDescending(a,b,c);
+	ASSERT_EQ(a, 3);
+	ASSERT_EQ(b, 2);
+	ASSERT_EQ(c, 1);
+}
+
+// sortDescending Sad Test
+TEST(PracticeTest, outside_sort)
+{
+	Practice testObject;
+	int a = 3;
+	int b = 2;
+  int c = 3;
+	testObject.sortDescending(a,b,c);
+	ASSERT_EQ(a, 3);
+	ASSERT_EQ(b, 3);
+	ASSERT_EQ(c, 2);
 }
